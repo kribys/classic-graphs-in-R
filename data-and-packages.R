25ce4ff43097ee686751a3811e6ee9f7c8992ef5
Kristian Bysheim <kristian@bysheim.info>
2021-05-12 12:58:39 +0200

    Moar commits.

--- data-and-packages.R
+++ data-and-packages.R
@@ -1,10 +1,11 @@
 if (!require("pacman")) install.packages("pacman")
-p_load(minard, HistData)
+p_load(HistData)
 
 # https://www.rdocumentation.org/packages/HistData/versions/0.8-6/topics/Minard
 # https://vincentarelbundock.github.io/Rdatasets/doc/HistData/Minard.temp.html
 # https://rdrr.io/rforge/HistData/man/Minard.html
 # https://www.cs.uic.edu/~wilkinson/TheGrammarOfGraphics/minard.txt
+
 
 data(Minard.troops)
 data(Minard.cities)
